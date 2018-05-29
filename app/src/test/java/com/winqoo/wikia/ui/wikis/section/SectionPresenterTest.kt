13496c0af7c2be86cd84701907423255d5e97c7f
Michal Winkler <mwinkler@Michals-MacBook-Pro.local>
2018-05-29 23:33:05 +0200

    tests for section presenter

--- app/src/test/java/com/winqoo/wikia/ui/wikis/section/SectionPresenterTest.kt
+++ app/src/test/java/com/winqoo/wikia/ui/wikis/section/SectionPresenterTest.kt
@@ -5,6 +5,7 @@ import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import com.winqoo.wikia.data.models.ExpandedWikiaItem
 import com.winqoo.wikia.data.models.ExpandedWikiaListResponse
+import com.winqoo.wikia.service.common.NoConnectivityException
 import com.winqoo.wikia.service.repository.WikiaRepository
 import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
@@ -41,15 +42,15 @@ class SectionPresenterTest {
     @Test
     fun fetchWikis_Success() {
 
-        val reponse = ExpandedWikiaListResponse()
-        reponse.batches = 1
-        reponse.currentBatch = 1
-        reponse.total = 10
-        reponse.currentBatch = 1
-        reponse.items = arrayOf(ExpandedWikiaItem(), ExpandedWikiaItem())
+        val response = ExpandedWikiaListResponse()
+        response.batches = 1
+        response.currentBatch = 1
+        response.total = 10
+        response.currentBatch = 1
+        response.items = arrayOf(ExpandedWikiaItem(), ExpandedWikiaItem())
 
         val single: Single<ExpandedWikiaListResponse> = Single.create { emitter ->
-            emitter.onSuccess(reponse)
+            emitter.onSuccess(response)
         }
 
         whenever(wikiaRepository.getListOfExpandedWikis(any())).thenReturn(single)
@@ -62,8 +63,8 @@ class SectionPresenterTest {
         verify(view, Mockito.times(1)).hideEmptyContent()
         verify(view, Mockito.times(1)).hideContent()
         verify(view, Mockito.times(1)).showProgress()
-        verify(view).addWikis(reponse.items?.toList()
-                ?: ArrayList() )
+        verify(view).addWikis(response.items?.toList()
+                ?: ArrayList())
         verify(view).showContent()
         verify(view).hideProgress()
 
@@ -71,15 +72,71 @@ class SectionPresenterTest {
 
     @Test
     fun fetchWikis_Success_Empty() {
+        val response = ExpandedWikiaListResponse()
+        response.batches = 1
+        response.currentBatch = 1
+        response.total = 10
+        response.currentBatch = 1
+        response.items = arrayOf()
+
+        val single: Single<ExpandedWikiaListResponse> = Single.create { emitter ->
+            emitter.onSuccess(response)
+        }
+
+        whenever(wikiaRepository.getListOfExpandedWikis(any())).thenReturn(single)
+
+        presenter.fetchWikis()
+
+        testScheduler.triggerActions()
+
+        verify(view, Mockito.times(1)).hideError()
+        verify(view, Mockito.times(1)).hideEmptyContent()
+        verify(view, Mockito.times(1)).hideContent()
+        verify(view, Mockito.times(1)).showProgress()
+        verify(view).showEmptyContent()
+        verify(view).hideProgress()
     }
 
     @Test
     fun fetchWikis_Success_Fail_No_Connection() {
+
+        val error = NoConnectivityException()
+        val single: Single<ExpandedWikiaListResponse> = Single.create { emitter ->
+            emitter.onError(error)
+        }
+
+        whenever(wikiaRepository.getListOfExpandedWikis(any())).thenReturn(single)
+        presenter.fetchWikis()
+
+        testScheduler.triggerActions()
+
+        verify(view, Mockito.times(1)).hideError()
+        verify(view, Mockito.times(1)).hideEmptyContent()
+        verify(view, Mockito.times(1)).hideContent()
+        verify(view, Mockito.times(1)).showProgress()
+        verify(view).hideProgress()
+        verify(view).showError(error)
     }
 
     @Test
     fun fetchWikis_Success_Fail_Server_Error() {
 
+        val error = Exception()
+        val single: Single<ExpandedWikiaListResponse> = Single.create { emitter ->
+            emitter.onError(error)
+        }
+
+        whenever(wikiaRepository.getListOfExpandedWikis(any())).thenReturn(single)
+        presenter.fetchWikis()
+
+        testScheduler.triggerActions()
+
+        verify(view, Mockito.times(1)).hideError()
+        verify(view, Mockito.times(1)).hideEmptyContent()
+        verify(view, Mockito.times(1)).hideContent()
+        verify(view, Mockito.times(1)).showProgress()
+        verify(view).hideProgress()
+        verify(view).showError(error)
     }
 
 }
